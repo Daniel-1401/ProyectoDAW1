ca073b90bf86d11d6862a0c5275d140faf6f1b99
Daniel <carlosdaniel-14@hotmail.com>
2021-12-02 13:32:32 -0500

    Inicio

---
+++ Comart/src/main/java/com/comartnet/comart/ComartApplication.java
@@ -0,0 +1,13 @@
+package com.comartnet.comart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComartApplication.class, args);
+	}
+
+}
